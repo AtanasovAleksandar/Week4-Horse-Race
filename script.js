f0634df75a0e431a8a501dba0958b4023f8ffdfc
Aleksandar Atanasov <aleksandaratanasov43@gmail.com>
2019-01-02 21:05:57 +0100

    race can beggin

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
-function Move() {
+function move() {
     alert('Race starting !')
     runHorse1();
     runHorse2();
+    runHorse3();
+    runHorse4();
 }
 
 function runHorse1() {
@@ -40,6 +42,45 @@ function runHorse2() {
         } else {
             pos++;
             horse2.style.left = pos + "px";
+        }
+    }
+}
+
+function runHorse3() {
+    var horse3 = document.getElementById("horse-3");
+    horse3.src = "img/run.gif";
+    var pos = 0;
+    var speed = Math.floor(Math.random() * 10);
+    console.log(speed)
+    var run = setInterval(move, speed);
+    function move() {
+        if (pos == 800) {
+            horse3.src = "img/stand.gif";
+            clearInterval(run);
+            alert('winner is player 3')
+        } else {
+            pos++;
+            horse3.style.left = pos + "px";
+        }
+    }
+}
+
+
+function runHorse4() {
+    var horse4 = document.getElementById("horse-4");
+    horse4.src = "img/run.gif";
+    var pos = 0;
+    var speed = Math.floor(Math.random() * 10);
+    console.log(speed)
+    var run = setInterval(move, speed);
+    function move() {
+        if (pos == 800) {
+            horse4.src = "img/stand.gif";
+            clearInterval(run);
+            alert('winner is player 4')
+        } else {
+            pos++;
+            horse4.style.left = pos + "px";
         }
     }
 }
