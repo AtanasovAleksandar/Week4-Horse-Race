516bbb1e0b2d367ce1e14e14aec9d693374cf109
Aleksandar Atanasov <aleksandar.atanasov@office.it-labs.com>
2019-01-02 16:51:04 +0100

    second horse

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-function myMove() {
-    alert('Race starting ! ')
+
+function Move() {
+    alert('Race starting !')
+    runHorse1();
+    runHorse2();
+}
+
+function runHorse1() {
     var horse = document.getElementById("horse-1");
     horse.src = "img/run.gif";
     var pos = 0;
     var speed = Math.floor(Math.random() * 10);
-    console.log(speed)
+    console.log(speed);
     var run = setInterval(move, speed);
     function move() {
         if (pos == 800) {
             horse.src = "img/stand.gif";
             clearInterval(run);
-            alert('winner is player 1')
+            alert('winner is player 1');
         } else {
             pos++;
             horse.style.left = pos + "px";
         }
     }
 }
+
+
+function runHorse2() {
+    var horse2 = document.getElementById("horse-2");
+    horse2.src = "img/run.gif";
+    var pos = 0;
+    var speed = Math.floor(Math.random() * 10);
+    console.log(speed)
+    var run = setInterval(move, speed);
+    function move() {
+        if (pos == 800) {
+            horse2.src = "img/stand.gif";
+            clearInterval(run);
+            alert('winner is player 2')
+        } else {
+            pos++;
+            horse2.style.left = pos + "px";
+        }
+    }
+}
+
+
